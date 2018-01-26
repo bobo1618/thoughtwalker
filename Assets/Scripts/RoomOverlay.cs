18ff3529e5706525984ef28087322b18bf9950a3
rohan.menon <rohan.menon@byjus.com>
2018-01-27 00:44:51 +0530

    Room Transitions

---
+++ Assets/Scripts/RoomOverlay.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomOverlay : MonoBehaviour {
+    private bool isEntered;
+    private bool isOccupied;
+	
+	// Update is called once per frame
+	void Update () {
+        float fadeAmount = 1f;
+
+        if(isEntered) {
+            fadeAmount = 0.5f;
+        }
+
+        if(isOccupied) {
+            fadeAmount = 0f;
+        }
+
+        Debug.Log("isEntered " + isEntered + "  isOccupied " + isOccupied);
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        Debug.Log(sRenderer.color.a + "Alpha");
+        Debug.Log(fadeAmount + "FadeAmount");
+        if(sRenderer.color.a < fadeAmount || sRenderer.color.a > fadeAmount) {
+            DoFade(fadeAmount);
+        }
+    }
+
+    public void DoFade(float fadeAmount) {
+        GetComponent<SpriteRenderer>().DOFade(fadeAmount, 1.0f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            isEntered = true;
+            isOccupied = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            isOccupied = false;
+        }
+    }
+}
