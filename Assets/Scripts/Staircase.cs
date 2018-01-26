129dbe90f9a36175f9255c27e8009159704d832d
rohan.menon <rohan.menon@byjus.com>
2018-01-26 23:44:53 +0530

    Initial Commit with movement and stairs

---
+++ Assets/Scripts/Staircase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Staircase : MonoBehaviour {
+    public Transform linkedStaircase;
+    private GameObject toTransport;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerStay2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            if(Input.GetKeyDown("w")) {
+                toTransport = collision.gameObject;
+                TransportObject();
+            }
+        }
+    }
+
+    public void TransportObject() {
+        toTransport.GetComponent<Collider2D>().enabled = false;
+        //toTransport.GetComponent<SpriteRenderer>().enabled = false;
+        toTransport.GetComponent<SpriteRenderer>().DOFade(0f, 1f);
+        toTransport.transform.DOMove(linkedStaircase.transform.position, 1f).OnComplete(() => HandleDoneTransport());
+    }
+
+    public void HandleDoneTransport() {
+        toTransport.GetComponent<SpriteRenderer>().DOFade(1f, 0.5f);
+        toTransport.GetComponent<Collider2D>().enabled = true;
+    }
+}
