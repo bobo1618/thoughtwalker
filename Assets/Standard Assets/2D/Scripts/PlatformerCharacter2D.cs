30d7f72ca61e75bcce4793e0f325e31827ac1eac
Projjal <projjal.chakrabarti@byjus.com>
2018-01-28 00:23:02 +0530

    Player character, v0.1

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+		[SerializeField] SpriteRenderer toFlip;
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
@@ -105,13 +106,9 @@ namespace UnityStandardAssets._2D
             // Switch the way the player is labelled as facing.
             m_FacingRight = !m_FacingRight;
 
-            // Multiply the player's x local scale by -1.
-
-            if(GetComponent<SpriteRenderer>().flipX) {
-                GetComponent<SpriteRenderer>().flipX = false;
-            } else {
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
+			// Multiply the player's x local scale by -1.
+			if (!toFlip) return;
+			toFlip.flipX = !toFlip.flipX;
         }
     }
 }
