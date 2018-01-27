e70c6584d70916a862c0ea5f3033a0572a305bb0
rohan.menon <rohan.menon@byjus.com>
2018-01-27 11:56:00 +0530

    Basic Thought System

---
+++ Assets/Scripts/ThoughtGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GGJ.Thoughts {
+    public enum EmotionalState
+    {
+        STAGE1,
+        STAGE2,
+        STAGE3
+    }
+
+    public class ThoughtGenerator : MonoBehaviour {
+        public Transform thoughtPosition;
+        public GameObject thoughtPrefab;
+        public Sprite thoughtSprite;
+        public float appearDelay;
+        public float fadeDelay;
+        public Transform parentTransform;
+        public EmotionalState thoughtState;
+        public bool autoFade;
+        public float thoughtDuration;
+
+        private GameObject thoughtGenerated;
+
+        void GenerateThought() {
+            if(thoughtGenerated != null) {
+                return;
+            }
+
+            if(parentTransform == null) {
+                thoughtGenerated = Instantiate(thoughtPrefab, thoughtPosition);
+            } else {
+                thoughtGenerated = Instantiate(thoughtPrefab, parentTransform);
+            }
+            thoughtGenerated.GetComponent<ThoughtScript>().PlayThought(appearDelay, thoughtSprite, autoFade);
+        }
+
+        void FadeThought() {
+            if(thoughtGenerated.activeInHierarchy) {
+                thoughtGenerated.GetComponent<ThoughtScript>().FadeThought();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if(other.gameObject.tag == "Player") {
+                Invoke("GenerateThought", 0f); // For now
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other) {
+            if(other.gameObject.tag == "Player") {
+                Invoke("FadeThought", fadeDelay);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThoughtScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace GGJ.Thoughts {
+    public class ThoughtScript : MonoBehaviour {
+        bool autoFade;
+
+        public void Update() {
+            SpriteRenderer sRendereer = GetComponent<SpriteRenderer>();
+            if(transform.localScale.x < 0) {
+                sRendereer.flipX = true;
+            } else {
+                sRendereer.flipX = false;
+            }
+        }
+
+        // Use this for initialization
+        public void PlayThought(float timeAppear, Sprite thoughtSprite, bool autoFade) {
+            SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+            Color spriteColor = sRenderer.color;
+            spriteColor.a = 0f;
+            sRenderer.color = spriteColor;
+            sRenderer.sprite = thoughtSprite;
+
+            if(autoFade) {
+                sRenderer.DOFade(1.0f, timeAppear).OnComplete(() => FadeThought());
+            } else {
+                sRenderer.DOFade(1.0f, timeAppear);
+            }
+        }
+
+        public void FadeThought() {
+            SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+            Tweener tweenFade = sRenderer.DOFade(0.0f, 0.5f).OnComplete(() => KillThought());
+        }
+
+        void KillThought() {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
