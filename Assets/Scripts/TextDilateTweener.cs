a7cbc3e3ddd21e7409e0df96d5067625ea94fb3b
Projjal <projjal.chakrabarti@byjus.com>
2018-01-28 12:53:20 +0530

    Journal fixes, text dilate tween

---
+++ Assets/Scripts/TextDilateTweener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TextDilateTweener : MonoBehaviour {
+	[SerializeField] Material textMat;
+	[SerializeField] float fromVal, toVal, durationMin, durationMax;
+
+	void Start () {
+		TweenDilate();
+	}
+
+	void TweenDilate() {
+		DOTween.To(() => fromVal, fd => textMat.SetFloat("_FaceDilate", fd), toVal, Random.Range(durationMin, durationMax)).SetLoops(2, LoopType.Yoyo).SetEase(Ease.Linear).OnComplete(() => TweenDilate());
+	}
+}
